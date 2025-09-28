e68b30b26ad7409fb98c2ca76a7f126328677e4a
Basma22Mostafa <basmagoda18@gmail.com>
2025-09-28 22:01:43 +0300

    fifth-commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
-import { getToken } from "next-auth/jwt";
+
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function middleware(request:NextRequest){
-    const token =await getToken({req:request});
+    const token =
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("_Secure-next-auth.session-token")?.value;
     if(token){
         return NextResponse.next();
     }else{
